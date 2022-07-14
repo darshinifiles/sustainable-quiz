8301a7b589d2f179c1334f50bc4ca8595d41768a
Tharik J.H <darshinifiles@gmail.com>
2022-07-15 03:38:52 +0800

    test

---
+++ js/first.js
@@ -0,0 +1,14 @@
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+
+function myFunction3() {
+  var popup = document.getElementById("myPopup3");
+  popup.classList.toggle("show");
+}
+
+function myFunction4() {
+  var popup = document.getElementById("myPopup4");
+  popup.classList.toggle("show");
+}
